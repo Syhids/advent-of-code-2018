1a3d1fa7724411122756bff23c10972b061598d3
Manuel Jiménez <mjimenez@habitissimo.com>
2018-12-31 13:35:38 +0100

    AoC 7: not very readable :/

---
+++ src/main/java/io/mjimenez/results/advent6/Advent7.kt
@@ -0,0 +1,22 @@
+package io.mjimenez.results.advent6
+
+import java.io.File
+
+fun main(args: Array<String>) {
+    val lines = File("input4").readLines()
+    val records = parseInput(lines)
+    val worklog = records.sorted().toWorklog()
+
+    val guardIdsToMinuteSlices = worklog.map { it.guardId to it.shifts.flatMap { shift -> shift.toMinuteSlices() } }
+
+    //There are some guards who don't fall asleep: #2063, #3109 and #2957
+
+    val (guardId, slices) = guardIdsToMinuteSlices.maxBy { (_, slices) ->
+        slices.groupBy { it }.values.maxBy { it.count() }?.size ?: 0
+    }!!
+    val minuteMostAsleep = slices.groupBy { it }.values.maxBy { it.count() }!!.first()
+
+    println("Guard: $guardId")
+    println("Minute most asleep: $minuteMostAsleep")
+    println("Result: ${guardId.toInt() * minuteMostAsleep}")
+}
